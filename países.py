1c3bb76ed9b66b4c5b1d26eefa5a6aeee305295a
mariaflorthomaz-source <mariaflorthomaz@gmail.com>
2025-10-13 15:02:36 -0300

    Update países.py

--- países.py
+++ países.py
@@ -3,3 +3,11 @@ import streamlit as st
 
 dataset = pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
 st.dataframe(dataset)
+
+fig = px.choropleth(dataset, 
+                     locations=dataset ['iso3'], 
+                     color=dataset ['nome'], 
+                     hover_name=dataset ['nome'])
+fig.update_layout(title='Mapa Coroplético dos Países',
+                  geo_scope='world')
+fig.show() 
