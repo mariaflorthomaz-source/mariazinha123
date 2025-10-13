3db672ed1abe447b4a01ddded92208df201dd51a
mariaflorthomaz-source <mariaflorthomaz@gmail.com>
2025-10-13 15:13:00 -0300

    Update países.py

--- países.py
+++ países.py
@@ -2,3 +2,18 @@ import pandas as pd
 import streamlit as st
 
 dataset = pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
+
+import pandas as pd
+import streamlit as st
+import  plotly.express as px
+
+dataset = pd.read_csv ("https://www.irdx.com.br/media/uploads/paises.csv")
+
+
+fig = px.choropleth(dataset,
+                   locations=dataset["iso3"],
+                    color=dataset["longitude"],
+                    hover_name=dataset["nome"])
+fig.update_layout(title="Mapa Coropletico dos Paises",
+                  geo_scope="world")
+st.plotly_chart(fig, use_container_width=True, theme="streamlit")
