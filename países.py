21d19e7ab41d6bfaf1bed5ca8fd30e47c131519e
mariaflorthomaz-source <mariaflorthomaz@gmail.com>
2025-10-13 15:13:38 -0300

    Update países.py

--- países.py
+++ países.py
@@ -1,10 +1,5 @@
 import pandas as pd
 import streamlit as st
-
-dataset = pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
-
-import pandas as pd
-import streamlit as st
 import  plotly.express as px
 
 dataset = pd.read_csv ("https://www.irdx.com.br/media/uploads/paises.csv")
